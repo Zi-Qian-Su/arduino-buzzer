2d7a98d7152050e9e017dbe88ada373b95341ae7
蘇子謙 <73123882+Zi-Qian-Su@users.noreply.github.com>
2021-01-27 20:33:03 +0800

    Update buzzer.ino

--- buzzer/buzzer.ino
+++ buzzer/buzzer.ino
@@ -9,5 +9,14 @@ void loop()
   delay(700);
   tone(8, 698, 1000);
   delay(700);
- 
+  tone(8, 523, 1000);
+  delay(700);
+  tone(8, 698, 1000);
+  delay(700);
+  tone(8, 523, 1000);
+  delay(700);
+  tone(8, 698, 1000);
+  delay(700);
+  noTone(8);//腳位8停止撥放
+  delay(2000);
 }
